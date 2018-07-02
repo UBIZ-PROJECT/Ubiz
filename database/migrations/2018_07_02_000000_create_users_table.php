8389e5b9316759e5a727986bd9c3e601507c6651
dtnhat3011 <dothanhnhat91@gmail.com>
2018-07-02 22:49:07 +0700

    fix create table users

--- database/migrations/2018_07_02_000000_create_users_table.php
+++ database/migrations/2018_07_02_000000_create_users_table.php
@@ -15,6 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
+			$table->string('user_name', 50);
             $table->char('password', 64);
 			$table->char('remember_token', 100);
             $table->string('name', 100);
