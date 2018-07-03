e77c53e327cd07ea608c1297028a56994209bc5d
hoaithy92 <hoaithy92@gmail.com>
2018-07-03 23:16:41 +0700

    create suppliers table by migration

---
+++ database/migrations/2018_07_03_161407_create_suppliers_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSuppliersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('suppliers', function (Blueprint $table) {
+            $table->increments('sup_id');
+            $table->string('sup_name', 100);
+            $table->char('sup_phone', 15);
+            $table->char('sup_fax', 20);
+            $table->string('sup_mail', 100);
+            $table->string('sup_website', 100);
+            $table->char('delete_flg', 1)->default('0');
+            $table->timestamp('inp_date');
+            $table->integer('inp_user');
+            $table->timestamp('upd_date')->useCurrent();
+            $table->integer('upd_user');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('suppliers');
+    }
+}
