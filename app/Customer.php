7d26ccde5cb77f2af27937b773eb4bfcdfe8f37b
dtnhat <dtnhat@brights.vn>
2018-07-12 10:26:37 +0700

    fix path

--- app/Customer.php
+++ app/Customer.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
-use Illuminate\support\Facades\DB;
+use Illuminate\Support\Facades\DB;
 
 class Customer implements JWTSubject
 {
