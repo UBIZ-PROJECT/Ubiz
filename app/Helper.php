24ef0d1eb645bba133585075cce81b59722386b1
dtnhat <dtnhat@brights.vn>
2018-07-12 10:24:30 +0700

    add image class

--- app/Helper.php
+++ app/Helper.php
@@ -1,8 +1,27 @@
 <?php
 
 namespace App;
+use App\SimpleImage;
+
 
 class Helper
 {
-
+	public static function resizeImage($img, $img_type, $target_img, $new_img_width, $new_img_height)
+	{
+			$image = new SimpleImage();
+			$target_dir = base_path() . '/public/images/' . $img_type . '/';
+			$target_file = $target_dir . $target_img);
+			$image->load($img);
+			$image->resize($new_img_width, $new_img_height);
+			$image->save($target_file);
+	}
+	
+	public static function readImage($img, $img_type)
+	{
+		$path   = base_path() . '/public/images/' . $img_type . '/' . $img;
+		$type   = pathinfo($path, PATHINFO_EXTENSION);
+		$data   = file_get_contents($path);
+		$base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
+		return $base64;
+	}
 }
