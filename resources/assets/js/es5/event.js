9f01243a0569a9938c88a8f519db1ea3751c98e2
itnomichi <38125513+itnomichi@users.noreply.github.com>
2019-06-11 22:52:49 +0700

    update event-management

--- resources/assets/js/es5/event.js
+++ resources/assets/js/es5/event.js
@@ -1,3 +1,29 @@
+var pic_list = new Array();
+
+var debounce = function (func, wait, immediate) {
+    var timeout;
+    return function () {
+        var context = this, args = arguments;
+        var later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
+
+function event_pic_search(self) {
+
+    if (pic_list.length == 0)
+        return false;
+
+    var match_list = _.filter(pic_list, {name: $(self).val()});
+    event_load_pic_callback(match_list);
+}
+
 function event_edit(info) {
     console.log(info);
     moment.locale('vi');
@@ -5,6 +31,16 @@ function event_edit(info) {
     $("#event-start-date").val(moment().format('MMM DD, YYYY'));
     $("#event-end-date").val(moment().format('MMM DD, YYYY'));
     $('#event-modal').modal();
+}
+
+function event_pic_select(self, event) {
+    event.stopPropagation();
+    var fa_check = $(self).find('div:first');
+    if (fa_check.hasClass('assigned')) {
+        fa_check.removeClass('assigned');
+    } else {
+        fa_check.addClass('assigned');
+    }
 }
 
 function event_add() {
@@ -15,27 +51,29 @@ function event_add() {
 }
 
 function event_load_pic() {
-    ubizapis('v1', 'events/pic', 'get', null, null, function (response) {
-        if (response.data.success == true) {
-            event_load_pic_callback(response.data.users);
-        } else {
-            swal.fire({
-                type: 'error',
-                title: response.data.message
-            });
-        }
-    });
+    if (pic_list.length == 0) {
+        ubizapis('v1', 'events/pic', 'get', null, null, function (response) {
+            if (response.data.success == true) {
+                pic_list = response.data.users;
+                event_load_pic_callback(response.data.users);
+            } else {
+                swal.fire({
+                    type: 'error',
+                    title: response.data.message
+                });
+            }
+        });
+    } else {
+        event_load_pic_callback(pic_list);
+    }
 }
 
 function event_load_pic_callback(users) {
+    var assigned_list = event_get_list_of_assigned_pic();
+    event_render_pic_dropdown_list(users, assigned_list);
+}
 
-
-    var assigned_list = {};
-    $(".assigned-list").find('li.list-group-item').each(function (idx, ele) {
-        var pic = $(ele).attr('pic');
-        assigned_list[pic] = pic;
-    });
-
+function event_render_pic_dropdown_list(users, assigned_list) {
     var html = "";
     $.map(users, function (user, idx) {
 
@@ -44,8 +82,8 @@ function event_load_pic_callback(users) {
             assigned_class = '';
         }
 
-        html += '<a pic="' + user.id + '" class="dropdown-item media pdl-5" href="#">';
-        html += '<div class="' + assigned_class + '" style="height: 30px; line-height: 30px">';
+        html += '<a pic="' + user.id + '" class="dropdown-item media pdl-5" href="#" onclick="event_pic_select(this, event)">';
+        html += '<div class="' + assigned_class + '" style="height: 30px; width: 20px; line-height: 30px">';
         html += '<i class="fas fa-check pdr-5"></i>';
         html += '</div>';
         html += '<div style="width: 30px; height: 30px" class="mr-3">';
@@ -62,6 +100,72 @@ function event_load_pic_callback(users) {
     });
     $(".event-pic").find('.dropdown-menu').empty();
     $(".event-pic").find('.dropdown-menu').html(html);
+}
+
+function event_get_list_of_assigned_pic() {
+
+    var assigned_list = {};
+    $(".assigned-list").find('li.list-group-item').each(function (idx, ele) {
+        var pic = $(ele).attr('pic');
+        assigned_list[pic] = pic;
+    });
+    return assigned_list;
+}
+
+function event_get_list_of_selected_pic() {
+
+    var assigned_list = {};
+    $(".event-pic").find('div.dropdown-menu').find('a').each(function (idx, ele) {
+        if ($(ele).find('div:first').hasClass('assigned')) {
+            var pic = $(ele).attr('pic');
+            assigned_list[pic] = pic;
+        }
+    });
+
+    var selected_list = new Array();
+    $.map(pic_list, function (user, idx) {
+
+        if (typeof assigned_list[user.id] !== "undefined") {
+            selected_list.push(user);
+        }
+    });
+    return selected_list;
+}
+
+function event_set_assigned_list() {
+    var assigned_list = event_get_list_of_selected_pic();
+    event_render_assigned_dropdown_list(assigned_list);
+}
+
+function event_render_assigned_dropdown_list(assigned_list) {
+    var html = "";
+    $.map(assigned_list, function (user, idx) {
+        html += '<li pic="' + user.id + '" class="border-top-0  border-bottom-0 list-group-item z-pdl z-pdr z-pdb" style="display: flex; align-items: flex-start;">';
+        html += '<div style="width: 30px; height: 30px" class="mr-3">';
+        html += '<img src="' + user.avatar + '" class="img-fluid img-thumbnail" alt="">';
+        html += '</div>';
+        html += '<div class="media-body" style="height: 30px; line-height: 5px">';
+        html += '<h6 class="mt-0 mb-1">' + user.name + '</h6>';
+        html += '<small>' + user.dep_name + '</small>';
+        html += '</div>';
+        html += '<div style="height: 30px; line-height: 30px">';
+        html += '<i class="fas fa-times pdr-5" onclick="event_delete_selected_pic(this)"></i>';
+        html += '</div>';
+        html += '</li>';
+    });
+    $(".assigned-list").empty();
+    $(".assigned-list").html(html);
+}
+
+function event_delete_selected_pic(self) {
+    $(self).closest('li.list-group-item').remove();
+}
+
+function event_pic_keyup(self) {
+    var that = self;
+    debounce(function () {
+        event_pic_search(that);
+    }, 100);
 }
 
 moment.locale('vi', {
@@ -201,5 +305,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
     $('.event-pic').on('show.bs.dropdown', function () {
         event_load_pic();
+    })
+
+    $('.event-pic').on('hide.bs.dropdown', function () {
+        event_set_assigned_list();
     })
 });
