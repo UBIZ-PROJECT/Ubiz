4dd6f102550583db2c0b8c719507e4dc11181932
itnomichi <38125513+itnomichi@users.noreply.github.com>
2019-09-08 00:07:13 +0700

    update quoteprices-v3

--- resources/views/quoteprice_search.blade.php
+++ resources/views/quoteprice_search.blade.php
@@ -1,66 +1,18 @@
 <div class="search">
     <div class="tyo" role="search">
         <div class="cyo">
-            <input id="fuzzy" spellcheck="false" autocomplete="off" placeholder="Tìm kiếm báo giá" value="" type="text"
+            <input id="fuzzy" spellcheck="false"
+                   autocomplete="off"
+                   placeholder="Tìm kiếm báo giá"
+                   value="" type="text"
                    onkeypress="jQuery.UbizOIWidget.w_fuzzy_search_handle_enter(event)">
         </div>
-        <button class="dyo dropdown-toggle" id="search-form" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></button>
-        <div class="dropdown">
-            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="search-form">
-                <div class="search-form">
-                    <div class="row z-mgr z-mgl">
-                        <div class="col-12">
-                            <div class="row justify-content-start mgb-10">
-                                <div class="col-auto">
-                                    <label style="min-width: 80px" class="text-primary">Mã báo giá</label>
-                                </div>
-                                <div class="col-auto">
-                                    <textarea style="resize: none"
-                                              is-change="false" placeholder="" id="s-qp-code"
-                                              class="input-textarea "></textarea>
-                                </div>
-                                <div class="col-auto">
-                                    <select class="s-drd">
-                                        <option value="1">Bằng</option>
-                                        <option value="1">Không bằng</option>
-                                        <option value="1">Chứa</option>
-                                        <option value="1">Không chứa</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="row justify-content-start mgb-10">
-                                <div class="col-auto">
-                                    <label style="min-width: 80px" class="text-primary">Ngày tạo</label>
-                                </div>
-                                <div class="col-auto">
-                                    <input type="text" style="width: 100px" class="form-control light-color custom-form-control" value="2019/08/10">
-                                </div>
-                                <div class="col-auto z-pdl z-pdr">
-                                    <label>~</label>
-                                </div>
-                                <div class="col-auto">
-                                    <input type="text" style="width: 100px" class="form-control light-color custom-form-control" value="2019/08/10">
-                                </div>
-                            </div>
-                            <div class="row justify-content-start mgb-10">
-                                <div class="col-auto">
-                                    <label style="min-width: 80px" class="text-primary">Hết hạn</label>
-                                </div>
-                                <div class="col-auto">
-                                    <input type="text" style="width: 100px" class="form-control light-color custom-form-control" value="2019/08/10">
-                                </div>
-                                <div class="col-auto z-pdl z-pdr">
-                                    <label>~</label>
-                                </div>
-                                <div class="col-auto">
-                                    <input type="text" style="width: 100px" class="form-control light-color custom-form-control" value="2019/08/10">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <button class="dyo" onclick="show_advance_searh_form()">
+            <svg focusable="false" height="24px" viewBox="0 0 24 24" width="24px" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7 10l5 5 5-5z"></path>
+                <path d="M0 0h24v24H0z" fill="none"></path>
+            </svg>
+        </button>
         <button class="jyo">
             <svg focusable="false" height="24px" viewBox="0 0 24 24" width="24px" xmlns="http://www.w3.org/2000/svg">
                 <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path>
@@ -74,4 +26,241 @@
             </svg>
         </button>
     </div>
+    <div id="search-form" class="eyo">
+        <div class="hvo">
+            <div class="row z-mgr z-mgl pdt-20">
+                <div class="col-12">
+                    <div class="row justify-content-start mgb-10">
+                        <div class="col-auto">
+                            <label style="min-width: 80px" class="text-primary">Mã báo giá</label>
+                        </div>
+                        <div class="col-auto">
+                                        <textarea style="resize: none"
+                                                  is-change="false" placeholder="" id="s-qp-code"
+                                                  class="input-textarea "></textarea>
+                        </div>
+                        <div class="col-auto">
+                            <select class="s-drd">
+                                <option value="1">Bằng</option>
+                                <option value="1">Không bằng</option>
+                                <option value="1">Chứa</option>
+                                <option value="1">Không chứa</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="row justify-content-start mgb-10">
+                        <div class="col-auto">
+                            <label style="min-width: 80px" class="text-primary">Ngày tạo</label>
+                        </div>
+                        <div class="col-auto">
+                            <input type="text" style="width: 100px"
+                                   class="form-control light-color custom-form-control" value="2019/08/10">
+                        </div>
+                        <div class="col-auto z-pdl z-pdr">
+                            <label>~</label>
+                        </div>
+                        <div class="col-auto">
+                            <input type="text" style="width: 100px"
+                                   class="form-control light-color custom-form-control" value="2019/08/10">
+                        </div>
+                    </div>
+                    <div class="row justify-content-start mgb-10">
+                        <div class="col-auto">
+                            <label style="min-width: 80px" class="text-primary">Hết hạn</label>
+                        </div>
+                        <div class="col-auto">
+                            <input type="text" style="width: 100px"
+                                   class="form-control light-color custom-form-control" value="2019/08/10">
+                        </div>
+                        <div class="col-auto z-pdl z-pdr">
+                            <label>~</label>
+                        </div>
+                        <div class="col-auto">
+                            <input type="text" style="width: 100px"
+                                   class="form-control light-color custom-form-control" value="2019/08/10">
+                        </div>
+                    </div>
+                    <div class="row justify-content-start mgb-10">
+                        <div class="col-auto">
+                            <label style="min-width: 80px" class="text-primary">Nhân viên</label>
+                        </div>
+                        <div class="col-auto dropdown">
+                            <ul class="sale-step multiple-select"
+                                id="dropdownMenuButton"
+                                data-toggle="dropdown"
+                                aria-haspopup="true"
+                                aria-expanded="false">
+                                <li>
+                                    <div class="multiple-select-spacer">&nbsp;,</div>
+                                    <div class="multiple-select-tag">Báo giá</div>
+                                    <div class="multiple-select-delete"><i></i></div>
+                                </li>
+                                <li>
+                                    <div class="multiple-select-spacer">&nbsp;,</div>
+                                    <div class="multiple-select-tag">Đơn hàng</div>
+                                    <div class="multiple-select-delete"><i></i></div>
+                                </li>
+                                <li>
+                                    <div class="multiple-select-spacer">&nbsp;,</div>
+                                    <div class="multiple-select-tag">Hợp đồng</div>
+                                    <div class="multiple-select-delete"><i></i></div>
+                                </li>
+                                <li>
+                                    <div class="multiple-select-spacer">&nbsp;,</div>
+                                    <div class="multiple-select-tag">Giao hàng</div>
+                                    <div class="multiple-select-delete"><i></i></div>
+                                </li>
+                            </ul>
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Another action</a>
+                                <a class="dropdown-item" href="#">Something else here</a>
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <select class="s-drd">
+                                <option value="1">Chứa</option>
+                                <option value="1">Không chứa</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="row justify-content-start mgb-10">
+                        <div class="col-auto">
+                            <label style="min-width: 80px" class="text-primary">Khách hàng</label>
+                        </div>
+                        <div class="col-auto dropdown">
+                            <ul class="sale-step multiple-select"
+                                id="dropdownMenuButton"
+                                data-toggle="dropdown"
+                                aria-haspopup="true"
+                                aria-expanded="false">
+                                <li>
+                                    <div class="multiple-select-spacer">&nbsp;,</div>
+                                    <div class="multiple-select-tag">Báo giá</div>
+                                    <div class="multiple-select-delete"><i></i></div>
+                                </li>
+                                <li>
+                                    <div class="multiple-select-spacer">&nbsp;,</div>
+                                    <div class="multiple-select-tag">Đơn hàng</div>
+                                    <div class="multiple-select-delete"><i></i></div>
+                                </li>
+                                <li>
+                                    <div class="multiple-select-spacer">&nbsp;,</div>
+                                    <div class="multiple-select-tag">Hợp đồng</div>
+                                    <div class="multiple-select-delete"><i></i></div>
+                                </li>
+                                <li>
+                                    <div class="multiple-select-spacer">&nbsp;,</div>
+                                    <div class="multiple-select-tag">Giao hàng</div>
+                                    <div class="multiple-select-delete"><i></i></div>
+                                </li>
+                            </ul>
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Another action</a>
+                                <a class="dropdown-item" href="#">Something else here</a>
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <select class="s-drd">
+                                <option value="1">Chứa</option>
+                                <option value="1">Không chứa</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="row justify-content-start mgb-10">
+                        <div class="col-auto">
+                            <label style="min-width: 80px" class="text-primary">Tổng tiền</label>
+                        </div>
+                        <div class="col-auto">
+                            <input type="text" style="width: 200px" value=""
+                                   class="form-control light-color custom-form-control">
+                        </div>
+                        <div class="col-auto">
+                            <select class="s-drd">
+                                <option value="1">Bằng</option>
+                                <option value="1">Không bằng</option>
+                                <option value="1">Nhỏ hơn</option>
+                                <option value="1">Lớn hơn</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="row justify-content-start mgb-10">
+                        <div class="col-auto">
+                            <label style="min-width: 80px" class="text-primary">Trạng thái</label>
+                        </div>
+                        <div class="col-auto dropdown">
+                            <ul class="sale-step multiple-select"
+                                id="dropdownMenuButton"
+                                aria-haspopup="true"
+                                aria-expanded="false">
+                                <li>
+                                    <div class="multiple-select-spacer">&nbsp;,</div>
+                                    <div class="multiple-select-tag">Báo giá</div>
+                                    <div class="multiple-select-delete"><i></i></div>
+                                </li>
+                                <li>
+                                    <div class="multiple-select-spacer">&nbsp;,</div>
+                                    <div class="multiple-select-tag">Đơn hàng</div>
+                                    <div class="multiple-select-delete"><i></i></div>
+                                </li>
+                                <li>
+                                    <div class="multiple-select-spacer">&nbsp;,</div>
+                                    <div class="multiple-select-tag">Hợp đồng</div>
+                                    <div class="multiple-select-delete"><i></i></div>
+                                </li>
+                                <li>
+                                    <div class="multiple-select-spacer">&nbsp;,</div>
+                                    <div class="multiple-select-tag">Giao hàng</div>
+                                    <div class="multiple-select-delete"><i></i></div>
+                                </li>
+                            </ul>
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Another action</a>
+                                <a class="dropdown-item" href="#">Something else here</a>
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <select class="s-drd">
+                                <option value="1">Chứa</option>
+                                <option value="1">Không chứa</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <div class="row z-mgr z-mgl pdb-20">
+                <div class="col-12 text-right">
+                    <button type="button" class="btn btn-link btn-sm text-secondary"
+                            onclick="clear_advance_searh_form()">{{ __("Clear filter") }}</button>
+                    <button type="button" class="btn btn-primary btn-sm"
+                            onclick="jQuery.UbizOIWidget.w_search()">{{ __("Search") }}</button>
+                    <button type="button" class="btn btn-danger btn-sm"
+                            onclick="hide_advance_searh_form()">{{ __("Close") }}</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+<script>
+
+    function show_advance_searh_form() {
+        jQuery("#search-form").fadeIn('fast', function () {
+            document.body.addEventListener('click', hide_advance_searh_form, false);
+        });
+    }
+
+    function hide_advance_searh_form(e) {
+        var search_form = jQuery(e.target).closest("#search-form");
+        if (search_form.length == 0) {
+            document.body.removeEventListener('click', hide_advance_searh_form, false);
+            jQuery("#search-form").hide('fast');
+        }
+    }
+
+    function clear_advance_searh_form() {
+        jQuery("#search-form").hide('fast');
+    }
+</script>
