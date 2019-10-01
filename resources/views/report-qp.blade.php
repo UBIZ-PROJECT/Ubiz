cc43cb430779f57103780ceb09d06299746a988e
Do Thanh Nhat <dothanhnhat91@gmail.com>
2019-10-01 22:10:32 +0700

    change layout for report rev and qp

--- resources/views/report-qp.blade.php
+++ resources/views/report-qp.blade.php
@@ -58,24 +58,22 @@
                                 <td style="padding: 10px">@include('components.input',['control_id'=>'report_to_date', 'value'=> date('Y/m/d'), 'width'=> '150', 'lbl_width'=>'70', 'label'=>'Đến ngày', 'class'=>'datepicker z-pdl z-pdr', 'i_focus'=>'', 'i_blur'=>'', 'onchange'=>"qp_date_change(this)"])</td>
                                 <td style="padding: 10px">@include('components.input',['control_id'=>'sale_name', 'value'=> '', 'width'=> '250', 'lbl_width'=>'70', 'label'=>'Nhân viên', 'length'=>'100'])</td>
                                 <td><span class="btn btn-info" id="statis-button" onclick="jQuery.UbizOIWidget.w_statis()"> Thống kê </span></td>
+                                <td><span class="btn btn-info export" id="rev-export-button" style="margin-left: 20px" onclick="jQuery.UbizOIWidget.w_export('quoteprice')"> Xuất excel </span></td>
                             </tr>
                         </table>
                     </form>
                     <table>
                         <tr>
-                            <td style="width: 280px"><p style="margin-top:10px; font-size:15px"><strong>Số lượng báo giá:</strong> <span id="report_count">{{ $paging['rows_num'] }}</span></p></td>
-                            <td style="width: 280px"><p style="margin-top:10px; font-size:15px"><strong>Tổng doanh thu dự kiến:</strong> <span id="report_sum">{{ $report->sum }}</span> ₫</p></td>
+                            <td style="width: 280px"><p style="font-size:15px"><strong>Số lượng báo giá:</strong> <span id="report_count">{{ $paging['rows_num'] }}</span></p></td>
+                            <td style="width: 310px"><p style="font-size:15px"><strong>Tổng doanh thu dự kiến:</strong> <span id="report_sum">{{ $report->sum }}</span> ₫</p></td>
                             <td></td>
                         </tr>
                         <tr>
-                            <td style="width: 280px"><p style="margin-top:10px; font-size:15px"><strong>Tổng tiền đã báo giá:</strong> <span id="total_qp_amount">{{ $report->total_qp_amount }}</span> ₫</p></td>
-                            <td style="width: 280px"><p style="margin-top:10px; font-size:15px"><strong>Tổng doanh thu đạt được:</strong> <span id="total_ord_amount">{{ $report->total_ord_amount }}</span> ₫</p></td>
-                            <td style="width: 280px"><p style="margin-top:10px; font-size:15px"><strong>Tỉ lệ thành công:</strong> <span id="success_rate">{{ (integer) str_replace(',', '', $report->total_qp_amount)?((integer) str_replace(',', '', $report->total_ord_amount) / (integer) str_replace(',', '', $report->total_qp_amount) * 100) : 0 }}</span> %</p></td>
+                            <td style="width: 280px"><p style="font-size:15px"><strong>Tổng tiền đã báo giá:</strong> <span id="total_qp_amount">{{ $report->total_qp_amount }}</span> ₫</p></td>
+                            <td style="width: 310px"><p style="font-size:15px"><strong>Tổng doanh thu đạt được:</strong> <span id="total_ord_amount">{{ $report->total_ord_amount }}</span> ₫</p></td>
+                            <td style="width: 200px"><p style="font-size:15px"><strong>Tỉ lệ thành công:</strong> <span id="success_rate">{{ (integer) str_replace(',', '', $report->total_qp_amount)?((integer) str_replace(',', '', $report->total_ord_amount) / (integer) str_replace(',', '', $report->total_qp_amount) * 100) : 0 }}</span> %</p></td>
                         </tr>
                     </table>
-                    <div class="export">
-                        <span class="btn btn-info export" id="rev-export-button" onclick="jQuery.UbizOIWidget.w_export('quoteprice')"> Xuất excel </span>
-                    </div>
                     <div class="aqH" role="presentation">
                         <div class="yTP" role="presentation">
                             <div class="clG">
--- resources/views/report-rev.blade.php
+++ resources/views/report-rev.blade.php
@@ -58,6 +58,7 @@
                                 <td style="padding: 10px">@include('components.input',['control_id'=>'report_to_date', 'value'=> date('Y/m/d'), 'width'=> '150', 'lbl_width'=>'70', 'label'=>'Đến ngày', 'class'=>'datepicker z-pdl z-pdr', 'i_focus'=>'', 'i_blur'=>'', 'onchange'=>"qp_date_change(this)"])</td>
                                 <td style="padding: 10px">@include('components.input',['control_id'=>'sale_name', 'value'=> '', 'width'=> '250', 'lbl_width'=>'70', 'label'=>'Nhân viên', 'length'=>'100'])</td>
                                 <td><span class="btn btn-info" id="statis-button" onclick="jQuery.UbizOIWidget.w_statis()"> Thống kê </span></td>
+                                <td><span class="btn btn-info export" style="margin-left: 20px" id="rev-export-button" onclick="jQuery.UbizOIWidget.w_export('revenue')"> Xuất excel </span></td>
                             </tr>
                         </table>
                     </form>
@@ -67,9 +68,6 @@
                             <td style="width: 280px"><p style="margin-top:10px; font-size:15px"><strong>Tổng doanh thu:</strong> <span id="report_sum">{{ $report->sum }}</span> ₫</p></td>
                         </tr>
                     </table>
-                    <div class="export">
-                        <span class="btn btn-info export" id="rev-export-button" onclick="jQuery.UbizOIWidget.w_export('revenue')"> Xuất excel </span>
-                    </div>
                     <div class="aqH" role="presentation">
                         <div class="yTP" role="presentation">
                             <div class="clG">
